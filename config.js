e3ebfa7f1bbcf5071296f21cca3ab39ffe254269
jason <jason@olingerhq.com>
2016-09-05 20:45:02 -0500

    second

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'secretKey': '44089-97530-05337-70097',
-    'mongoUrl': 'mongodb://localhost:27017/wikiMed',
-    'facebook': {
-        clientID: '670641833102183',
-        clientSecret: 'cea87824549a943249b5c1afa79cfc95',
-        callbackURL: 'https://localhost:3443/users/facebook/callback'
-    }
-}
